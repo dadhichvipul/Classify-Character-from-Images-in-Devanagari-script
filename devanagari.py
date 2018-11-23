51e06627c2ab27328a128f1a514c32208704913a
dadhichvipul <37115907+dadhichvipul@users.noreply.github.com>
2018-11-23 17:20:42 +0530

    Update devanagari.py

--- devanagari.py
+++ devanagari.py
@@ -1,129 +1,67 @@
-from __future__ import division
-import numpy
-import scipy.special
+from __future__ import division, print_function, absolute_import
+import numpy as np
+from six.moves import cPickle as pickle
+from six.moves import range
 
-class neuralNetwork:
-    
-    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
-        #number of input nodes
-        self.input_nodes = inputnodes
-        #number of hidden nodes
-        self.hidden_nodes = hiddennodes
-        #number of output nodes
-        self.output_nodes = outputnodes
-        #learning rate
-        self.lr = learningrate
-        
-        #random weight of links between input and hidden layer (in range of -0.5 to +0.5)
-        self.weight_input_hidden = (numpy.random.rand(self.hidden_nodes, self.input_nodes) - 0.5)
-        #random weight of links between hidden and output layer (in range of -0.5 to +0.5)
-        self.weight_hidden_output = (numpy.random.rand(self.output_nodes, self.hidden_nodes) - 0.5)
-        
-        #sigmoid function
-        self.activation_function = lambda x: scipy.special.expit(x) 
-    
-    def train(self, inputs_list, targets_list):
-        
-        #convert inputs and targets list to 2d array
-        inputs = numpy.array(inputs_list, ndmin=2).T
-        targets = numpy.array(targets_list, ndmin=2).T
-        
-        #inputs to hidden layer
-        hidden_inputs = numpy.dot(self.weight_input_hidden, inputs)
-        #outputs from hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
-        
-        #inputs to output layer
-        final_inputs = numpy.dot(self.weight_hidden_output, hidden_outputs)
-        #outputs from output layer
-        final_outputs = self.activation_function(final_inputs)
-        
-        #error between target value and observed value of the output layer
-        output_errors = targets - final_outputs
-        
-        #error for the hidden layer via backpropagation
-        hidden_errors = numpy.dot(self.weight_hidden_output.T, output_errors)
-        
-        #gradient descent to update the weights
-        #update the weights of the links between hidden and output layer
-        self.weight_hidden_output += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
-        
-        #update the weights of the links between the input and hidden layer
-        self.weight_input_hidden += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
-    
-    def predict(self, inputs_list):
-        
-        #convert input list to 2d array
-        inputs = numpy.array(inputs_list, ndmin=2).T
-        
-        #inputs to hidden layer
-        hidden_inputs = numpy.dot(self.weight_input_hidden, inputs)
-        
-        #outputs from the hidden layer
-        hidden_outputs = self.activation_function(hidden_inputs)
-        
-        #inputs to output layer
-        final_inputs = numpy.dot(self.weight_hidden_output, hidden_outputs)
-        
-        #outputs from the output layer
-        final_outputs = self.activation_function(final_inputs)
-        
-        return final_outputs
-        
-input_nodes = 1024
-hidden_nodes = 300
-output_nodes = 10
-learning_rate = 0.3
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten,Convolution2D, MaxPooling2D, Dropout
+from keras.optimizers import SGD
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import EarlyStopping
 
-neural = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+batch_size = 128
+nb_classes = 46
+nb_epoch = 12
 
-# load the mnist train data CSV file
-training_data = open("train.csv", 'r')
-training_list = training_data.readlines()
-training_data.close()
+pickle_file = 'devanagari.pickle'
 
-for record in training_list:
-    all_values = record.split(',')
-    #prepreocess the pixels in order to scale them in between 0.01 to 1.00
-    inputs = (numpy.asfarray(all_values[1:]) / 255 * 0.99 ) + 0.01 
-    
-    #target labels. all values are 0.01 except the correct label which has a value of 0.99
-    targets = numpy.zeros(output_nodes) + 0.01
-    targets[int(all_values[0])] = 0.99
-    
-    #begin the training
-    neural.train(inputs, targets)
-    
-# load the mnist test data CSV file
-test_data = open("test.csv", 'r')
-test_list = test_data.readlines()
-test_data.close()
+with open(pickle_file, 'rb') as f:
+  save = pickle.load(f)
+  train_dataset = save['train_dataset']
+  train_labels = save['train_labels']
+  valid_dataset = save['valid_dataset']
+  valid_labels = save['valid_labels']
+  test_dataset = save['test_dataset']
+  test_labels = save['test_labels']
+  del save  # hint to help gc free up memory
+  print('Training set', train_dataset.shape, train_labels.shape)
+  print('Validation set', valid_dataset.shape, valid_labels.shape)
+  print('Test set', test_dataset.shape, test_labels.shape)
 
-scores = []
-for record in test_list:
-    all_values = record.split(',')
-    
-    #the first value is the label
-    correct_label = int(all_values[0])
-    
-    #all the others are the pixels (i.e inputs)
-    inputs = (numpy.asfarray(all_values[1:]) / 255 * 0.99 ) + 0.01
-    
-    #use the trained network to predict the output based on the given input
-    outputs = neural.predict(inputs)
-    
-    #the index of the highest value in output is the predicted label
-    label = numpy.argmax(outputs)
+def reformat(dataset, labels):
+  dataset = dataset.reshape((-1, 1, 32, 32)).astype(np.float32)
+  # Map 0 to [1.0, 0.0, 0.0 ...], 1 to [0.0, 1.0, 0.0 ...]
+  labels = (np.arange(nb_classes) == labels[:,None]).astype(np.float32)
+  return dataset, labels
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
 
-    #if the correct label, and the predicted label are same
-    if (label == correct_label):
-        #append 1 to scores.
-        scores.append(1)
-    else:
-        #otherwise, wrong prediction, append 0 to scores.
-        scores.append(0)
-        pass
-    pass
+model = Sequential()
 
-scores_array = numpy.asarray(scores)
-print "Accuracy = ", (scores_array.sum() / scores_array.size) * 100, "%"
+model = Sequential()
+model.add(Convolution2D(100, 5, 5, border_mode='valid', input_shape=(1, 32, 32)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Activation('tanh'))
+model.add(Convolution2D(250, 5, 5, border_mode='valid'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Activation('tanh'))
+model.add(Flatten())
+model.add(Dense(1000))
+model.add(Dropout(0.5))
+model.add(Activation('relu'))
+model.add(Dense(46))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
+
+model.fit(train_dataset, train_labels, batch_size=batch_size, nb_epoch=nb_epoch,
+          verbose=1, validation_data=(valid_dataset, valid_labels))
+score = model.evaluate(test_dataset, test_labels, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
